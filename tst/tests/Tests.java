1ebde9760289844329c5d8fcd77fad143509156c
Dantiéris Castilhos Rabelini <dantieris@gmail.com>
2015-06-13 16:33:37 -0300

    Implementacao do teste #6.

--- tst/tests/Tests.java
+++ tst/tests/Tests.java
@@ -2,13 +2,22 @@ package tests;
 
 import static org.junit.Assert.*;
 
+import org.freemars.FreeMarsLauncher;
+import org.freemars.controller.FreeMarsController;
+import org.freemars.editor.EditorModel;
+import org.freerealm.Realm;
+import org.freerealm.executor.command.InitializeRealmCommand;
 import org.junit.Test;
 
 public class Tests {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testAbrirTelaDeEdicaoDeMapaNaoLancaNullPointerException() {
+		try {
+			new EditorModel().execute(new InitializeRealmCommand(new Realm()));
+		} catch (NullPointerException ex) {
+			fail("Não deveria lançar nenhuma exceção");
+		}
 	}
 
 }
